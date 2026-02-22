ee02b5b93b67dcd56de804eadcce036bb642207a
JoakimCarlsson <joakimcarlsson1994@gmail.com>
2026-02-22 15:03:50 +0100

    refactor: adjust Sidebar width and remove unused ActivityBar component

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -2,7 +2,6 @@ import { createRootRouteWithContext, Outlet } from "@tanstack/react-router"
 import type { QueryClient } from "@tanstack/react-query"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { AppStatusProvider } from "@/contexts/AppStatusContext"
-import { ActivityBar } from "@/components/layout/ActivityBar"
 import { Sidebar } from "@/components/layout/Sidebar"
 import { StatusBar } from "@/components/layout/StatusBar"
 
@@ -20,7 +19,6 @@ function RootLayout() {
       <TooltipProvider>
         <div className="flex h-screen flex-col bg-background font-sans antialiased">
           <div className="flex flex-1 min-h-0">
-            <ActivityBar />
             <Sidebar />
             <main className="flex-1 min-h-0 min-w-0">
               <Outlet />
